4b53017fd4e2a27965594fe922327df47490e60a
colbyy2x <s12057691@students.duvalschools.org>
2024-09-06 12:02:05 -0400

    added loops

---
+++ 00_computer_science_exercises/02_flow_control_structures/flowControlStructures.py/looooooops.py
@@ -0,0 +1,32 @@
+# Loops, JaColby Allen, v0.0
+
+#  TWO TYPES OF LOOPS 
+# for <-- Used when you kno whow many loops you'll need.
+# while <-- Used when you do not know how many loops you'll need.
+
+# for loop is like Go Fish, you search each card for what the player asked.
+# while loop is like musical chairs, you move around until the music stops.
+
+# EACH TRIP THROUGH THE ENTIRE LOOP IS CALLED AN iteration
+# "iterate throgh the list" means use a for loop
+
+# for loop Example -- Iterating a List
+# fruits =["apple", " banana", "kiwi", "orange"]
+# for eachFruit in fruits:
+#     print(eachFruit)
+
+#continue Keyword -- Skips the current iteration and then finishes the loop.
+# fruits =["apple", " banana", "kiwi", "orange"]
+# for eachFruit in fruits:
+#     if eachFruit== "banana":
+#         continue
+#     print(eachFruit)
+
+# break Keyword -- Immediately exit the loop.
+fruits =["apple", " banana", "kiwi", "orange"]
+for eachFruit in fruits:
+    if eachFruit== "banana":
+        break
+    print(eachFruit)
+
+

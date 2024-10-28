4b5108ec9c1e1cc30fa1ea208b3abdd30f43c592
colbyy2x <s12057691@students.duvalschools.org>
2024-10-28 13:05:19 -0400

    Nothing
    aAAAAAA

--- 00_computer_science_exercises/05_functions/functionsPractice.py/functionspractice.py
+++ 00_computer_science_exercises/05_functions/functionsPractice.py/functionspractice.py
@@ -5,17 +5,30 @@ import random
 def helloWorldMulti(): # FUNCTION SIGNATURE
     """ This function will output Hello, World! in a language the user choose.""" # DOCSTRING \
     # print a list of languages to the screen, at least three but no more than five.
-    print("The languages avaliable English, Spanish, French.\n")
+    print("The avaliable languages are English, Spanish, French")
     
     # allow the user to select (input) a choice for the language.
     lanaguage = input("What langauge do you want?")
-    if lanaguage = English print (" You have selected English, is that correct?")
-    if langauge = Spanish print ( "You have selected Spanish, is that correct?")
-    if language = French print (" You have selected French, is that correct?")
     
     # print " Hello, World!" to the screeb in that lanaguage 
-    print
+    if language == "English":
+        print("In English:\nHello, World!\n")
+    elif langauge == " Spanish":
+        print("In Spanish:\nHola, Mundo!\n")
+    elif language == "French":
+        print("In French:\nBon jour, Le Monde!\n")
+    else:
+        print("In Somali: \n, Haye caalamkow!\n")
 
+helloWorldMulti() # FUNCTION CALL
 
+# Function tp Detetmine Even / Odd Numbers 
+argument1 = random.randint(-1000, 1000)
 
-
+def isEven(argument: int) -> bool # Requires one ARGUMENT (argument1) and RETURNS a Boolean value.
+    """Determines is an integer value is even or odd."""
+    if argument % 2 ==0:
+        return True
+    else:
+        return False 
+isEven()

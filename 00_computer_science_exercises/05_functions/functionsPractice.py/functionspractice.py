714e9c4cf5e546da8023a749c1ab0fdc5c956c3b
colbyy2x <s12057691@students.duvalschools.org>
2024-10-28 12:04:55 -0400

    Function practice

---
+++ 00_computer_science_exercises/05_functions/functionsPractice.py/functionspractice.py
@@ -0,0 +1,21 @@
+# Functions Practice, Jacolby Allen, v0.0
+
+import random
+
+def helloWorldMulti(): # FUNCTION SIGNATURE
+    """ This function will output Hello, World! in a language the user choose.""" # DOCSTRING \
+    # print a list of languages to the screen, at least three but no more than five.
+    print("The languages avaliable English, Spanish, French.\n")
+    
+    # allow the user to select (input) a choice for the language.
+    lanaguage = input("What langauge do you want?")
+    if lanaguage = English print (" You have selected English, is that correct?")
+    if langauge = Spanish print ( "You have selected Spanish, is that correct?")
+    if language = French print (" You have selected French, is that correct?")
+    
+    # print " Hello, World!" to the screeb in that lanaguage 
+    print
+
+
+
+

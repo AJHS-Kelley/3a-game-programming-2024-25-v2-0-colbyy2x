a92eedbe69ef4cf6fc9c2321c05b20ec3a52b7ad
colbyy2x <s12057691@students.duvalschools.org>
2024-11-22 12:59:11 -0500

    Finished game

--- 01_gaming_exercises/05_dragon_Realm/dragonRealm.py
+++ 01_gaming_exercises/05_dragon_Realm/dragonRealm.py
@@ -3,6 +3,8 @@
 import random
 import time
 import datetime 
+pickWeaponUp = " yes or no"
+fightBack = "yes or no "
 
 # SAVING DATA TO A FILE 
 # STEP 1-- Create thefile name to use.
@@ -11,7 +13,7 @@ logFileName="DraginRealmLog" + str(time.time()) + ".txt"
 # Example: dragonRealmLog1132AM.txt
 
 # STEP 2 -- CREATE / OPEN THE FILE TO SAVE THE DATA
-saveData = open(logFileName, "x")
+saveData = open(logFileName, "a")
 #FILE MODES 
 #"X" CREATES FILE, IF FILE EXIST, EXIT WITH ERROR MESSAGE
 #"W" CREATE FILE, IF FILE EXIST, ERASE AND OVERWRITE FILE CONTENTS
@@ -23,6 +25,8 @@ def displayIntro():
     print('One random night you hear something outside')
     print('You take a look outside and see a dark shadowy figure flash infront of you')
     print('You start to run but only have three paths and one weapon to choose from')
+    print('If you dont choose the right path you will not have the option to fight back')
+    print('sooo.......')
     print('Choose wisely.')
     print()
 
@@ -37,26 +41,47 @@ def checkPath(chosenPath):
     if chosenPath != 1:
         print('You approach an alley')
         time.sleep(2)
-        print('Lights are flickering on and off ')
+        print('Street lights are flickering on and off ')
         time.sleep(2)
         print("")
+    else:
+        chosenPath = 2
+        print("You have reached a house")
+        time.sleep(2)
+        print("You open the door, you are now faced with more demons")
+        time.sleep(2)
+      
 
-    friendlyPath = random.randint("1")
-    DeadlyPath = random.randint("2")
+
+    friendlyPath = random.randint(1,2)
+    DeadlyPath = random.randint(2,3)
+    closet = ''
     
     if chosenPath == str(DeadlyPath):
         print('You have ran into a building with more demons')
-
+        print('You can run into the closet')
+        time.sleep(2)
+        print('Would you like to hide in the closet?')
+        print('Yes or no?')
+        closet = input()
+        print("You run into the closet")
+        time.sleep(2)
+        print("There is a sowrd in there with you.")
+        time.sleep(2)
+        print("Would you like to pick it up? Yes or no")
+        time.sleep(2)
+        pickWeaponUp = input()
+        print("You have picked the weapon.")
+        time.sleep(2)
+        print("You have the option to fight back. Would you like to? Yes or no?")
+        fightBack = input()
+        print(" Congrats You have killed the demon")
     else:
         print('You get eaten by demons')
         print('Game over ')
     
     if chosenPath == str(friendlyPath):
         print('You have made it away from the demon ')
-
-    else:
-        print('You bleed out and become a demon')
-        print('Game over')
 
 
 
@@ -69,5 +94,9 @@ while playAgain == 'yes' or playAgain == 'y':
     print('Do you want to play again? (yes or no)')
     playAgain = input()
 
+playAgain = 'no'
+print("Thank you for playing")
+
 # CLOSE THE FILE 
+saveData.write(r"END OF GAME LOG\N\N")
 saveData.close()
